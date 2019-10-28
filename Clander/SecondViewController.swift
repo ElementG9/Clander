f822210b05cf79b75cf1c3f2a5342d21382f7d50
ElementG9 <elementg9dev@gmail.com>
2019-10-28 09:42:51 -0600

    Initial Commit

---
+++ Clander/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Clander
+//
+//  Created by Garen Tyler on 10/28/19.
+//  Copyright © 2019 North Star Charter School. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
